918af530bc10d011acc49b1b2f0276c9a0782d10
DMOC1211 <145091883+DMOC1211@users.noreply.github.com>
2026-03-18 12:44:13 -0700

    Update car.py

--- car.py
+++ car.py
@@ -2,7 +2,7 @@
 Name: Daniel Puerto & Jacob Miranda
 Date: 3/17/26
 Group: 13
-Description:
+Description: Creates the class for Car and creates the special move and calculates the distance for the special move
 '''
 
 
@@ -11,15 +11,19 @@ import random
 
 class Car(Vehicle):
   def special_move(self, obs_loc):
+    #Special move is created that accelerates the car 
     if self.energy < 15:
             self._location += 1
             return f"{self.name} did not have enough energy for nitro and moved 1 space."
 
+    #Deducts energy
     self._energy -= 15
-    
+
+    #Calculates distance
     distance = int(1.5 * self.speed) + random.randint(-1, 1)
     target = self.location + distance
 
+    #If there is an obstacle, car crashes
     if obs_loc is not None and obs_loc <= target:
       self._location = obs_loc
       return (
@@ -27,5 +31,6 @@ class Car(Vehicle):
             f"at {obs_loc}!"
       )
 
+    #If the nitro boost is successful
     self._location = target
     return f"{self.name} used nitro and moved {distance} spaces."
