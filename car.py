c5212d15a1fbe571e9217dfadc4f7839f42b3f52
DMOC1211 <145091883+DMOC1211@users.noreply.github.com>
2026-03-17 23:24:22 -0700

    Update car.py

---
+++ car.py
@@ -0,0 +1,31 @@
+'''
+Name:
+Date:
+Group:
+Description:
+'''
+
+
+import vehicle
+import random 
+
+class Car(Vehicle):
+  def special_move(self, obs_loc):
+    if self.energy < 15:
+            self._location += 1
+            return f"{self.name} did not have enough energy for nitro and moved 1 space."
+
+    self._energy -= 15
+    
+    distance = int(1.5 * self.speed) + random.randint(-1, 1)
+    target = self.location + distance
+
+    if obs_loc is not None and obs_loc <= target:
+      self._location = obs_loc
+      return (
+            f"{self.name} used nitro for {distance} spaces but CRASHED at obstacle "
+            f"at {obs_loc}!"
+      )
+
+    self._location = target
+    return f"{self.name} used nitro and moved {distance} spaces."
