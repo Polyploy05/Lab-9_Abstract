fa9ed6bc37765fdf939686e22cd5e706e3949879
DMOC1211 <145091883+DMOC1211@users.noreply.github.com>
2026-03-18 12:40:25 -0700

    Update motorcycle.py

--- motorcycle.py
+++ motorcycle.py
@@ -12,22 +12,47 @@ import random
 
 class Motorcycle(Vehicle):
 
+  def slow(self, obs_loc):
+    base = int(self.speed * 0.75)
+    distance = base + random.randint(-1,1)
+    if distance < 1:
+      distance = 
+
+    target = self._location + distance 
+
+    if obs_loc <= target:
+      moved = obs_loc - self._location - 1
+      if moved < 1:
+        moved = 1
+      self._location += moved
+      return f"{self.name} moved slowly around an obstacle for {moved} spaces."
+    
+    #No obstacle
+    self._location = target
+    return f"{self.name} moved slowly for {distance} spaces."
+
   def special_move(self, obs_loc):
 
-    if self.energy < 10:
+    if self.energy < 15:
       self._location += 1
       return f"{self.name} did not have enough energy and moved 1 space."
 
-    if random.randint(0,1) == 0:
-      return f"{self.name} attempted a stunt but FELL OVER and did not move!"
+    self._energy -= 15
 
-    self._energy -= 10
+    if random.random() >= 0.75:
+
+      self._location += 1
+      return f"{self.name} attempted a stunt but FELL OVER and moved only 1 space!"
+
 
     distance = (2 * self.speed) + random.randint(-1, 1)
+    if distance < 1:
+      distance = 1
+      
     target = self.location + distance
 
 
-    if obs_loc is not None and obs_loc <= target:
+    if obs_loc <= target:
       self._location = obs_loc
       return(
           f"{self.name} preformed a stunt for {distance} spaces but CRASHED " 
